f99670c8529d42c44aaa098a5ee57ed6ca31ca44
AdrienBarrau <90207129+AdrienBarrau@users.noreply.github.com>
2024-06-20 00:05:57 +0200

    Update classifier_random_forrest.py

--- classifier_random_forrest.py
+++ classifier_random_forrest.py
@@ -54,7 +54,8 @@ def get_text_features(text, lexique_dict):
     avg_word_length = sum(len(word) for word in words) / num_words if num_words > 0 else 0
     avg_syllables_per_word = num_syllables / num_words if num_words > 0 else 0
 
-    # Ajouter les caractéristiques lexiques
+    # Ajoutez des caracteristiques
+    
     freqs = [lexique_dict[word]['freqlemlivres'] if word in lexique_dict else 0 for word in words]
     avg_freq = sum(freqs) / num_words if num_words > 0 else 0
     if len(freqs) < 2:
@@ -67,7 +68,7 @@ def get_text_features(text, lexique_dict):
 
 
 
-# Donnees importantes
+
 lexique = lexique[['ortho', 'cgram', 'freqlemlivres']]
 
 
@@ -87,30 +88,28 @@ for level in levels:
         texts.append(text)
         labels.append(level)
         avg_word_length, avg_syllables_per_word,avg,q1,q2,q3,q4 = get_text_features(text,lexique)
-        features.append([avg_word_length, avg_syllables_per_word,avg,q1,q2,q3,q4])
+        features.append([avg_word_length, avg_syllables_per_word,avg,q1,q2,q3,q4])   # on utilise les quantiles des frequences
 
 vectorizer = TfidfVectorizer()
 X_tfidf = vectorizer.fit_transform(texts)
 
-# Convertir les nouvelles caractéristiques en tableau numpy
+
 features = np.array(features)
 
 
-# Combiner les caractéristiques TF-IDF, et les caractéristiques lexicales
 X_combined = np.hstack((X_tfidf.toarray(), features))
 
-# Utiliser SMOTE pour équilibrer les classes (si nécessaire)
+
 smote = SMOTE(random_state=42)
 X_resampled, y_resampled = smote.fit_resample(X_combined, labels)
 
-# Séparer les données en ensemble d'entraînement et de test de manière stratifiée
+
 X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.2, stratify=y_resampled, random_state=42)
 
-# Construire le modèle Random Forest avec des hyperparamètres ajustés
+
 model = RandomForestClassifier(n_estimators=500, max_depth=50, random_state=42)  #mieux: n_est,max_depth=(500,50) ou (600,55) , Acc=0.54
 model.fit(X_train, y_train)
 
-# Évaluer le modèle
 y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f'Accuracy: {accuracy:.2f}')
@@ -118,13 +117,13 @@ print(classification_report(y_test, y_pred))
 
 
 
-# Fonction pour transformer les caractéristiques du texte
+
 def transform_text_features(texts, vectorizer, get_text_features, lexique_dict):
-    # Vectoriser les textes avec TF-IDF
+ 
     X_tfidf = vectorizer.transform(texts)
-    # Convertir les nouvelles caractéristiques en tableau numpy
+
     features = np.array([get_text_features(text, lexique) for text in texts])
-    # Combiner les caractéristiques TF-IDF et les nouvelles caractéristiques
+
     X_combined = np.hstack((X_tfidf.toarray(), features))
     return X_combined
 
@@ -133,7 +132,7 @@ joblib.dump(vectorizer, 'tfidf_vectorizer.pkl')
 joblib.dump(get_text_features, 'get_text_features.pkl')
 
 
-# Fonction pour prédire le niveau CECRL d'un nouveau texte
+# Fonction qui predit le niveau CECRL d'un nouveau texte
 def predict_cefr_level(text):
     text_combined = transform_text_features([text], vectorizer, get_text_features, lexique)
     prediction = model.predict(text_combined)
@@ -164,14 +163,9 @@ niveau_pred = predict_cefr_level(texte1)
 print(f'Le niveau CECRL prédit pour le texte est: {niveau_pred}')
 
 
-# app.py
-#!pip install flask gradio joblib pandas scikit-learn imblearn
-import joblib
-import pandas as pd
-import numpy as np
-import statistics
-import gradio as gr
-from flask import Flask, request, jsonify
+
+
+# Application 
 
 app = Flask(__name__)
 
@@ -180,59 +174,8 @@ model = joblib.load('random_forest_cefr_model.pkl')
 vectorizer = joblib.load('tfidf_vectorizer.pkl')
 get_text_features = joblib.load('get_text_features.pkl')
 
-lexique_path = '/Lexique383.tsv'
-lexique = pd.read_csv(lexique_path, delimiter='\t')
 
 
-# Fonctions auxiliaires
-def count_syllables(word):
-    voyelles = "aeiouyAEIOUY"
-    n = len(word)
-    if all(char in voyelles for char in word):
-        return 1
-
-    nb_syllabes = 0
-    precedent_voyelle = False
-    for i, char in enumerate(word):
-        if char in voyelles:
-            if i == 0 or not precedent_voyelle:
-                nb_syllabes += 1
-            precedent_voyelle = True
-        else:
-            precedent_voyelle = False
-    return nb_syllabes
-
-def get_text_features(text, lexique_dict):
-    words = text.split()
-    num_words = len(words)
-    num_syllables = sum(count_syllables(word) for word in words)
-    avg_word_length = sum(len(word) for word in words) / num_words if num_words > 0 else 0
-    avg_syllables_per_word = num_syllables / num_words if num_words > 0 else 0
-
-    freqs = [lexique_dict[word]['freqlemlivres'] if word in lexique_dict else 0 for word in words]
-    avg_freq = sum(freqs) / num_words if num_words > 0 else 0
-    if len(freqs) < 2:
-        return avg_word_length, avg_syllables_per_word, avg_freq if freqs else 0, 0, 0, 0, 0
-    else:
-        quantiles = statistics.quantiles(freqs, n=5)
-        return avg_word_length, avg_syllables_per_word, avg_freq, *quantiles
-
-    return avg_word_length, avg_syllables_per_word, avg_freq, quantiles[0], quantiles[1], quantiles[2], quantiles[3]
-
-# Fonction pour transformer les caractéristiques du texte
-def transform_text_features(texts, vectorizer, get_text_features, lexique_dict):
-    X_tfidf = vectorizer.transform(texts)
-    features = np.array([get_text_features(text, lexique_dict) for text in texts])
-    X_combined = np.hstack((X_tfidf.toarray(), features))
-    return X_combined
-
-# Fonction pour prédire le niveau CECRL d'un nouveau texte
-def predict_cefr_level(text):
-    text_combined = transform_text_features([text], vectorizer, get_text_features, lexique)
-    prediction = model.predict(text_combined)
-    return prediction[0]
-
-# Endpoint pour prédire le niveau CECRL via une requête POST
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
@@ -240,12 +183,12 @@ def predict():
     niveau_pred = predict_cefr_level(text)
     return jsonify({'niveau': niveau_pred})
 
-# Interface utilisateur avec Gradio
+
 def predict_and_display(text):
     niveau_pred = predict_cefr_level(text)
     return f'Le niveau CECRL prédit pour le texte est: {niveau_pred}'
 
-# Créer l'interface Gradio
+# interface Gradio
 iface = gr.Interface(
     fn=predict_and_display,
     inputs=gr.Textbox(lines=10, placeholder="Entrez le texte ici..."),
@@ -254,18 +197,18 @@ iface = gr.Interface(
     description="Entrez un texte en français pour prédire son niveau CECRL (A1, A2, B1, B2, C1, C2)."
 )
 
-# Lancer l'interface Gradio dans une fonction de thread séparé
+
 def launch_gradio():
     iface.launch(share=True)
 
 if __name__ == "__main__":
     from threading import Thread
 
-    # Lancer l'interface Gradio dans un thread séparé
+
     thread = Thread(target=launch_gradio)
     thread.start()
 
-    # Lancer l'application Flask
+
     app.run(debug=True, use_reloader=False)
 
 
